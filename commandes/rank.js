e90ff9f879e3aa9657886f0e829f728a76a9b2fb
Arthur Sasaki <jesseoyekan@gmail.com>
2024-09-21 17:10:49 +0000

    Update rank.js

--- commandes/rank.js
+++ commandes/rank.js
@@ -211,7 +211,7 @@ france( {
     
     
          let msg = `
-┏━━━┛ FLASH-MD Ranking ┗━━━┓
+┏━━━┛ Gon-freecss-Md Ranking ┗━━━┓
      
   *Name :* ${nomAuteurMessage}
 
@@ -252,7 +252,7 @@ france( {
     const {ms , arg, repondre,auteurMessage,nomAuteurMessage, msgRepondu , auteurMsgRepondu , mybotpic} = commandeOptions ;
 
 
-       let msg = `┏━━┛ FLASH-MD-top-rang ┗━━┓\n\n`
+       let msg = `┏━━┛ Gon-freecss-Md-top-rang ┗━━┓\n\n`
        
       let topRanks = await getBottom10Users() ;
         let mention = [] ;
